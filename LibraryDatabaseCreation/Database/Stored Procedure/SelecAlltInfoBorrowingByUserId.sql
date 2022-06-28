7523ebee2be871b4f7dfeab167e102db5ffb3eca
Tanguy <tanguy_job@hotmail.fr>
2022-06-28 16:04:07 +0200

    SelectAllInfoBorrowingByUserId

--- LibraryDatabaseCreation/Database/Stored Procedure/SelecAlltInfoBorrowingByUserId.sql
+++ LibraryDatabaseCreation/Database/Stored Procedure/SelecAlltInfoBorrowingByUserId.sql
@@ -1,8 +1,7 @@
 ﻿CREATE PROCEDURE [dbo].[SelectAllInfoBorrowingByUserId]
 	@Id int
 AS
-	SELECT b.Id as borrowingId, b.DateBack, b.DateBegin, b.Additional, c.Id as CopyId, bo.Id as BookId, bo.Title,bo.[Resume], bo.PublicationDate,
-	u.Id as userId, u.Name as userName,u.Firstname,u.Birthdate, u.Email, u.[Password], u.Phone, u.IsActive, u.FK_User_Address, u.FK_User_Subscription, u.FK_User_Profile
+	SELECT b.Id as borrowingId, b.DateBack, b.DateBegin, b.Additional, c.Id as CopyId, bo.Id as BookId, bo.Title,bo.[Resume], bo.PublicationDate
 	from Borrowing b
 	join [Copy] c
 	on b.FK_Borrowing_Copy = c.Id
